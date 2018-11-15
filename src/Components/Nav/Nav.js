29ec73cafe399a9c7460fa1ff6d300b017258b34
lloydpolley <lloydpolley@googlemail.com>
2018-11-15 14:33:10 +0000

    json data

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -4,9 +4,12 @@ import './Nav.css';
 import {NavLink} from 'react-router-dom';
 
 class Nav extends Component {
+  constructor(props){
+    super(props);
+  }
   render() {
     return (
-      <div className="App">
+      <div className="navigation">
         <NavLink to="/">Home</NavLink>
         <NavLink to="/all">All Items</NavLink>
         <NavLink to="/basket">My Basket</NavLink>
